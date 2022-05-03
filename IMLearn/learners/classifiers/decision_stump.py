84eab005ca98735f7cb09cf99f509196a47e2b74
Dor <dormesika2@gmail.com>
2022-05-03 14:49:50 +0300

    EX.4 First init

---
+++ IMLearn/learners/classifiers/decision_stump.py
@@ -0,0 +1,138 @@
+from __future__ import annotations
+from typing import Tuple, NoReturn
+from ...base import BaseEstimator
+import numpy as np
+from itertools import product
+from ...metrics.loss_functions import misclassification_error
+
+
+class DecisionStump(BaseEstimator):
+    """
+    A decision stump classifier for {-1,1} labels according to the CART algorithm
+
+    Attributes
+    ----------
+    self.threshold_ : float
+        The threshold by which the data is split
+
+    self.j_ : int
+        The index of the feature by which to split the data
+
+    self.sign_: int
+        The label to predict for samples where the value of the j'th feature is about the threshold
+    """
+    def __init__(self) -> DecisionStump:
+        """
+        Instantiate a Decision stump classifier
+        """
+        super().__init__()
+        self.threshold_, self.j_, self.sign_ = None, None, None
+
+    def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
+        """
+        fits a decision stump to the given data
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Input data to fit an estimator for
+
+        y : ndarray of shape (n_samples, )
+            Responses of input data to fit to
+        """
+        min_err = np.inf
+        fit_thr, fit_j, sign = 0, 0, 0
+        d = X.shape[1]  # Number of features
+        for sign, j in product([-1, 1], range(d)):  # Nested loop
+            thr, thr_err = self._find_threshold(X[:, j], y[:, j], sign)
+            if thr_err < min_err:
+                fit_thr, fit_j, fit_sign_ = thr, j, sign
+                min_err = thr
+        self.threshold_, self.j_, self.sign_ = fit_thr, fit_j, sign
+
+    def _predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict responses for given samples using fitted estimator
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Input data to predict responses for
+
+        y : ndarray of shape (n_samples, )
+            Responses of input data to fit to
+
+        Returns
+        -------
+        responses : ndarray of shape (n_samples, )
+            Predicted responses of given samples
+
+        Notes
+        -----
+        Feature values strictly below threshold are predicted as `-sign` whereas values which equal
+        to or above the threshold are predicted as `sign`
+        """
+        return self.sign_ * (X[:, self.j_] <= self.threshold_)
+
+    def _find_threshold(self, values: np.ndarray, labels: np.ndarray, sign: int) -> Tuple[float, float]:
+        """
+        Given a feature vector and labels, find a threshold by which to perform a split
+        The threshold is found according to the value minimizing the misclassification
+        error along this feature
+
+        Parameters
+        ----------
+        values: ndarray of shape (n_samples,)
+            A feature vector to find a splitting threshold for
+
+        labels: ndarray of shape (n_samples,)
+            The labels to compare against
+
+        sign: int
+            Predicted label assigned to values equal to or above threshold
+
+        Returns
+        -------
+        thr: float
+            Threshold by which to perform split
+
+        thr_err: float between 0 and 1
+            Misclassificaiton error of returned threshold
+
+        Notes
+        -----
+        For every tested threshold, values strictly below threshold are predicted as `-sign` whereas values
+        which equal to or above the threshold are predicted as `sign`
+        """
+        # Sorting given data by values and fixing labels vector accordingly.
+        ordered_values = values.argsort()
+        values, labels = values[ordered_values], labels[ordered_values]
+        thr = values[0]
+        thr_err = np.sum(np.array([1 for label in labels if label != sign]))
+        for i in range(1, len(values)):
+            cur_loss = np.sum(np.array([1 for label in labels[i:] if label != sign])) + \
+                np.sum(np.array([1 for label in labels[:i] if label == sign]))
+            if cur_loss < thr_err:
+                thr_err = cur_loss
+                thr = values[i]
+        return float(thr), float(thr_err / labels.size)
+
+
+    def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
+        """
+        Evaluate performance under misclassification loss function
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            Test samples
+
+        y : ndarray of shape (n_samples, )
+            True labels of test samples
+
+        Returns
+        -------
+        loss : float
+            Performance under missclassification loss function
+        """
+        return misclassification_error(y, self._predict(X))
